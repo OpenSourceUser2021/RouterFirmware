1dfa7e5008e748c0a28e89e7c73632e17cf34685
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2021-11-05 22:26:06 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -9,6 +9,16 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
+# Set language to zh_cn
+uci set luci.main.lang='zh_cn'
+
+# set static lan and disable DHCP on lan
+uci set network.lan.proto='static'
+uci set network.lan.ipaddr='192.168.1.250'
+uci set network.lan.gateway='192.168.1.1'
+uci set network.lan.dns='192.168.1.1'
+uci set dhcp.lan.ignore='1'
+
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
 uci commit
