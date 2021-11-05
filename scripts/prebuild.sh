db9ceddd62ede4cf30a7d35db6284ed17ccff142
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2021-11-05 18:14:51 +0800

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir -p files/etc/uci-defaults/
 cp ../scripts/init-settings.sh files/etc/uci-defaults/99-init-settings
 mkdir -p files/etc/opkg
@@ -10,4 +11,4 @@ cp bin/packages/$PLATFORM/base/{ipv6helper*,odhcpd-ipv6only*,odhcp6c*,6in4*} "fi
 cp bin/targets/$TARGET/$SUBTARGET/packages/{ip6tables*,kmod-nf-nat6*,kmod-ipt-nat6*,kmod-sit*,kmod-ip6tables-extra*} "files/www/ipv6-modules"
 mkdir -p files/bin
 cp ../scripts/ipv6-helper.sh files/bin/ipv6-helper
-[ -e files ] && mv files openwrt/files
+# [ -e files ] && mv files openwrt/files
