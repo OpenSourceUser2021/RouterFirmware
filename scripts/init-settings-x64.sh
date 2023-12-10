c48f00dbf64f9ecca2e405a2a7f6d7ba7dc4f30e
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2023-12-10 22:26:01 +0800

    Update init-settings-x64.sh

--- scripts/init-settings-x64.sh
+++ scripts/init-settings-x64.sh
@@ -14,11 +14,11 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 uci set luci.main.lang='zh_cn'
 
 # set static lan and disable DHCP on lan
-uci set network.lan.proto='static'
-uci set network.lan.ipaddr='192.168.1.250'
-uci set network.lan.gateway='192.168.1.1'
-uci set network.lan.dns='192.168.1.1'
-uci set dhcp.lan.ignore='1'
+# uci set network.lan.proto='static'
+# uci set network.lan.ipaddr='192.168.1.250'
+# uci set network.lan.gateway='192.168.1.1'
+# uci set network.lan.dns='192.168.1.1'
+# uci set dhcp.lan.ignore='1'
 
 # login TTYD w/o password
 uci set ttyd.@ttyd[0].command='/bin/login -f root'
@@ -35,8 +35,8 @@ sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
 cp -r /usr/share/openclash/yacd/ /usr/share/openclash/dashboard/
 
 # fix system log issue "daemon.err modprobe: - bpfilter"
-cd /lib/modules/5.*/
-mv bpfilter.ko bpfilter.ko.bak
+# cd /lib/modules/5.*/
+# mv bpfilter.ko bpfilter.ko.bak
 
 # Disable IPV6 ula prefix
 sed -i 's/^[^#].*option ula/#&/' /etc/config/network
