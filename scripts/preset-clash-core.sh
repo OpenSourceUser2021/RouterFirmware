c3581977472fed0c0f0d1cc35635a3694e9196af
OpenSourceUser2021 <32152540+OpenSourceUser2021@users.noreply.github.com>
2024-05-14 10:30:53 +0800

    Update preset-clash-core.sh

--- scripts/preset-clash-core.sh
+++ scripts/preset-clash-core.sh
@@ -17,14 +17,14 @@ CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}' | grep -v 'v3')
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
 GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat"
-GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite-lite.dat"
-MMDB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
+#MMDB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/country-lite.mmdb"
 
 #wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
 #wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
-wget -qO- $MMDB_URL > files/etc/openclash/Country.mmdb
+#wget -qO- $MMDB_URL > files/etc/openclash/Country.mmdb
 # 给内核权限
 chmod +x files/etc/openclash/core/clash*
